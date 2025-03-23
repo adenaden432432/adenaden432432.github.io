6cf04e6037be12227f632355bb92414d70751b41
adenaden432432 <2248561681@qq.com>
2025-03-23 19:56:43 +0800

    添加日常板块

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'awards', 'experience','books'];
+const section_names = ['home', 'awards', 'experience','books','daily'];
 
 
 window.addEventListener('DOMContentLoaded', event => {
